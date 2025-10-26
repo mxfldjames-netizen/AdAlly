9efff16dd7314fc541358e5c3841370bdb4c2a74
mxfldjames-netizen <mxfldjames@gmail.com>
2025-10-26 22:02:45 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ const videoData = [
     title: "Lamborghini Huracan",
     thumbnail:
       "https://assets.zyrosite.com/dWxOrW2p4OtD7WKY/shot1-00910-mxB7Wb7OqRtNbpXD.png",
-    video: "https://www.youtube.com/embed/QmhYvijIrZ0",
+    video: "https://youtu.be/dCr_STXffWU",
     orientation: "landscape",
   },
   {
