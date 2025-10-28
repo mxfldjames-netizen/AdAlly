b38ed9d31a97132bfd83a18ad736d017d4e1f590
mxfldjames-netizen <mxfldjames@gmail.com>
2025-10-28 18:18:26 +0530

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -92,7 +92,7 @@ const HomePage: React.FC<HomePageProps> = ({
         {/* Stats Section - Animated Cards with Counters */}
         <div className="relative grid grid-cols-1 sm:grid-cols-3 gap-4 sm:gap-6 px-4 w-full max-w-4xl mx-auto"> 
           <AnimatedStatsCard 
-            icon={Zap} 
+            icon={Zap}  
             number="50+" 
             label="ADS CREATED" 
             delay={0} 
